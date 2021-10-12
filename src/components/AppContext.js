77ae56905fcbe02dd68bc61c3646b128634bdaf8
vishalbabburi <vishalbabburi@gmail.com>
2021-10-12 19:35:48 -0400

    added request dat

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -19,7 +19,7 @@ export const AppProvider = ({ children }) => {
     axios.post("http://localhost:5000/final", data).then(
       (response) => {
         setTestResponse(response.data);
-        console.log("response data: ", testResponse.group);
+        console.log("response data: ", response.data[group]);
       },
       (error) => {
         console.log(error);
