b4b367f337e147dc1337cce8497db7fc3df13d85
vishalbabburi <vishalbabburi@gmail.com>
2021-10-17 16:57:03 -0400

    added bu

--- src/components/output/Output.js
+++ src/components/output/Output.js
@@ -67,7 +67,7 @@ const Output =(props)=>{
                 <p>Interpretation: <b>{" "+testResponse.suggest+" "}</b></p>
                 <p> Total Cell Count <b> {": " +testResponse.count}  </b></p>
                 <br/>
-                <button type='button'className="btn btn-success" onClick={downloadFile(testResponse.key)}>Download</button>
+                <button type='button'className="btn btn-success" onClick={(testResponse)=>downloadFile(testResponse.key)}>Download</button>
                 <button onClick={() => window.location.reload(false)} className="btn btn-danger functionalButton" position = "absolute">  Log Out  </button>
                 <button onClick={testAgainHandler} className="btn btn-info functionalButton" position = "absolute">  Test Again  </button>  
 
