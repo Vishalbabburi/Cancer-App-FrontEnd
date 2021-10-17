2b6d6e54944f1db30787c5a7a07de5818aa04e31
vishalbabburi <vishalbabburi@gmail.com>
2021-10-17 17:08:49 -0400

    added key

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -32,8 +32,9 @@ export const AppProvider = ({ children }) => {
     );
   };
 
-  const downloadfileAsync = async (data) => {
-    console.log(data+" is being sent for /download")
+  const downloadfileAsync = async (outputKey) => {
+    console.log(outputKey+" is being sent for /download")
+    let data={key:outputKey}
     return axios.post('http://localhost:5000/download',data, {
         responseType: 'blob',
     })
